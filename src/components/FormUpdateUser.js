b289a6de1c65ca8827c08e051049568b8086180b
juancarlos <juanc.camacaro@gmail.com>
2025-05-18 14:38:10 -0500

    Form edit user accessibility fix 3

--- src/components/FormUpdateUser.js
+++ src/components/FormUpdateUser.js
@@ -71,7 +71,7 @@ function UpdateUserForm({ userId, onClose }) {
     return (
         <div className="overlay1">
             <div className="update-user">
-                <form autoFocus role="region" aria-labelledby="form-title" className="form-update-user" onSubmit={handleSubmit}>
+                <form tabIndex="0" autoFocus role="region" aria-labelledby="form-title" className="form-update-user" onSubmit={handleSubmit}>
                     <p id="form-title">Actualizar perfil</p>
                     <label>
                         Nombre:
